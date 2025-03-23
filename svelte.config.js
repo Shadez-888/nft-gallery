febc71ecf28171d92ebbc2e35c3ca64191288c4c
nami2111/ <aimsomnia@proton.me>
2025-03-24 02:48:09 +0900

    Update base path

--- svelte.config.js
+++ svelte.config.js
@@ -13,7 +13,7 @@ const config = {
 			strict: false
 		}),
 		paths: {
-			base: dev ? '' : '/nft-gallery'
+			base: dev ? '' : '/'
 		},
 		prerender: {
 			handleHttpError: ({ path, referrer, message }) => {
