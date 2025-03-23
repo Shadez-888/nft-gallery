3ac9abaa83c3e6a54e370bbf6a9a3933a227835b
nami2111/ <aimsomnia@proton.me>
2025-03-23 23:12:29 +0900

    Fix package dependencies and GitHub Actions workflow

--- svelte.config.js
+++ svelte.config.js
@@ -4,13 +4,19 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 const dev = process.env.NODE_ENV === 'development';
 
 const config = {
-	preprocess: vitePreprocess(),
 	kit: {
-		adapter: adapter(),
+		adapter: adapter({
+			pages: 'build',
+			assets: 'build',
+			fallback: 'index.html',
+			precompress: false,
+			strict: false
+		}),
 		paths: {
-			base: dev ? '' : '/nft-gallery' // Replace with your repository name
+			base: dev ? '' : '/nft-gallery'
 		}
-	}
+	},
+	preprocess: vitePreprocess()
 };
 
 export default config;
